13dc7da404a77c0bae44312609c020de851fa43d
Bryan Omar Ramirez Chavez <bryan@ezzeker.com>
2016-05-26 12:39:31 -0500

    Avances en login

--- Operacion_login.class.php
+++ Operacion_login.class.php
@@ -17,14 +17,20 @@ public function asignar($a,$b){
 }
 
 public function validar($p_user,$p_pass){
-	$sql = "select usu_pass from usuario where usu_nombre ='$p_user'";
+	$sql = "select usu_pass from usuario where usu_login ='$p_user'";
 	$rs = mysql_query($sql);
+ $contador = 0;
 	while($row = mysql_fetch_assoc($rs)){
+   $contador++;
  if ($p_pass == $row['usu_pass']){
  	$this-> respuesta = "Procede....";
  }
- else  $this->respuesta = "Contrasenia Incorrecta.";
+ else  $this-> respuesta = "Contrasenia Incorrecta.";
 	}
+	if ($contador == 0){
+$this-> respuesta = "Usuario no existe";
+	}
+
 }
 
 public function mostrar(){
